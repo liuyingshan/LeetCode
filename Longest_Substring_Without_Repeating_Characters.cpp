0ee9369c56404773d5ce326f3bd97632e3b5a20f
liuyingshan <liuyingshan070505@gmail.com>
2014-11-24 21:52:53 +0800

    Add 'Longest Substring Without Repeating Characters' solution.

---
+++ Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,34 @@
+/* 
+  Longest Substring Without Repeating Characters
+
+  Given a string, find the length of the longest substring without repeating
+  characters. For example, the longest substring without repeating letters for
+  "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring
+  is "b", with the length of 1.
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // String only contains ASCII characters.
+        // The function of array isAppeared equals to hash.
+        bool isAppeared[256];
+        for (int i=0; i<256; i++)
+            isAppeared[i] = false;
+
+		// To maintain a sliding window, in where each characters at most appears
+		// once.
+        int l = 0, r = 0, result = 0;
+        while (r < s.length()) {
+            while (isAppeared[s[r]]) {
+                isAppeared[s[l]] = false;
+                l++;
+            }
+            isAppeared[s[r]] = true;
+            if (r - l + 1 > result)
+                result = r - l + 1;
+            r++;
+        }
+        return result;
+    }
+};
